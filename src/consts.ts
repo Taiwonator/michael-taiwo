2d10bd2c21c5c665572decffe175949827b4d8b6
Michael Taiwo <22432591+Taiwonator@users.noreply.github.com>
2025-05-08 12:40:07 +0100

    feat(navigation): updates header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Michael Taiwo';
+export const SITE_DESCRIPTION = 'A space for my face and thoughts.';
